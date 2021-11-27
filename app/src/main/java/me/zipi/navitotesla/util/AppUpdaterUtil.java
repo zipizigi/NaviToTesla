e3d0db9bb0ed994dfef223b3e1e4c19afa3905eb
zipi <6834751+zipizigi@users.noreply.github.com>
2021-11-27 20:55:58 +0900

    오류, 안내 수정 이전에 요청한 목적지 안내 메시지 제거. (이동 중 지속적으로 노티 발생) 티맵의 경우 2개의 알림이 뜰 때가 있음(티맵버튼), 경로 안내만 체크하도록 변경 업데이트 안내 한글 적용

--- app/src/main/java/me/zipi/navitotesla/util/AppUpdaterUtil.java
+++ app/src/main/java/me/zipi/navitotesla/util/AppUpdaterUtil.java
@@ -1,11 +1,15 @@
 package me.zipi.navitotesla.util;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 
 import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.enums.Display;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
 
+import androidx.appcompat.app.AlertDialog;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -13,18 +17,33 @@ import lombok.NoArgsConstructor;
 public class AppUpdaterUtil {
 
     public static void notification(final Context context) {
-        new AppUpdater(context)
-                .setDisplay(Display.NOTIFICATION)
-                .setUpdateFrom(UpdateFrom.GITHUB)
-                .setGitHubUserAndRepo("zipizigi", "NaviToTesla")
-                .start();
+        appUpdater(context, Display.NOTIFICATION);
     }
 
     public static void dialog(Context context) {
+        appUpdater(context, Display.DIALOG);
+    }
+
+    private static void appUpdater(Context context, Display display) {
         new AppUpdater(context)
-                .setDisplay(Display.DIALOG)
+                .setDisplay(display)
                 .setUpdateFrom(UpdateFrom.GITHUB)
                 .setGitHubUserAndRepo("zipizigi", "NaviToTesla")
+
+                .setTitleOnUpdateAvailable("업데이트가 있습니다")
+                .setContentOnUpdateAvailable("새로운 업데이트가 있습니다. 아래 버튼을 눌러 업데이트 해주세요.")
+                .setButtonUpdate("업데이트")
+                .setButtonDismiss("닫기")
+                .setButtonDoNotShowAgain("업데이트 무시")
+                .setButtonDoNotShowAgainClickListener((d, w) ->
+                        new AlertDialog.Builder(context)
+                                .setTitle("안내")
+                                .setMessage("업데이트를 무시한 후 다시 업데이트 안내를 받고 싶다면 '주소 캐시 삭제' 버튼을 눌러주세요.")
+                                // .setIcon(R.drawable.ic_launcher_background)
+                                .setCancelable(false)
+                                .setPositiveButton("확인", (dialog, which) -> {
+                                })
+                                .show())
                 .start();
     }
 }
