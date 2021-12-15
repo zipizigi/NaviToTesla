a32cba5bddc5f523d17ba2a765eca068af91836b
zipi <itv5053@gmail.com>
2021-12-15 11:40:27 +0900

    Fix update error

--- app/src/main/java/me/zipi/navitotesla/api/TeslaApi.java
+++ app/src/main/java/me/zipi/navitotesla/api/TeslaApi.java
@@ -1,9 +1,7 @@
 package me.zipi.navitotesla.api;
 
-import me.zipi.navitotesla.model.TeslaApiResponse;
-import me.zipi.navitotesla.model.TeslaRefreshTokenRequest;
 import me.zipi.navitotesla.model.ShareRequest;
-import me.zipi.navitotesla.model.Token;
+import me.zipi.navitotesla.model.TeslaApiResponse;
 import me.zipi.navitotesla.model.Vehicle;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -12,8 +10,6 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface TeslaApi {
-    @POST("/oauth2/v3/token")
-    Call<Token> refreshAccessToken(@Body TeslaRefreshTokenRequest request);
 
     @GET("/api/1/vehicles")
     Call<TeslaApiResponse.ListType<Vehicle>> vehicles();
