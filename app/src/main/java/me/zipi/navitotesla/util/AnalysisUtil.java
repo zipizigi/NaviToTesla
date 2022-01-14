4c5ee7f93b0e0fc4fc3c4f9380e7d854ce7f38fe
zipi <6834751+zipizigi@users.noreply.github.com>
2022-01-14 14:05:13 +0900

    버전을 길게 눌러 로그파일을 볼 수 있는 기능 추가

--- app/src/main/java/me/zipi/navitotesla/util/AnalysisUtil.java
+++ app/src/main/java/me/zipi/navitotesla/util/AnalysisUtil.java
@@ -2,15 +2,27 @@ package me.zipi.navitotesla.util;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AnalysisUtil {
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
     private static final FirebaseCrashlytics firebaseCrashlytics = FirebaseCrashlytics.getInstance();
     private static FirebaseAnalytics firebaseAnalytics;
 
@@ -24,10 +36,31 @@ public class AnalysisUtil {
 
     public static void log(String message) {
         AnalysisUtil.firebaseCrashlytics.log(message);
+        appendLog("INFO", message);
+    }
+
+    public static void info(String message) {
+        AnalysisUtil.firebaseCrashlytics.log(message);
+        appendLog("INFO", message);
+    }
+
+    public static void warn(String message) {
+        AnalysisUtil.firebaseCrashlytics.log(message);
+        appendLog("WARN", message);
+    }
+
+    public static void error(String message) {
+        AnalysisUtil.firebaseCrashlytics.log(message);
+        appendLog("ERROR", message);
     }
 
     public static void recordException(Throwable e) {
         AnalysisUtil.firebaseCrashlytics.recordException(e);
+
+        try (PrintWriter writer = new PrintWriter(new StringWriter())) {
+            e.printStackTrace(writer);
+            appendLog("WARN", e.toString() + System.lineSeparator() + writer.toString());
+        }
     }
 
     public static void setCustomKey(String key, String value) {
@@ -36,5 +69,47 @@ public class AnalysisUtil {
 
     public static void sendUnsentReports() {
         AnalysisUtil.firebaseCrashlytics.sendUnsentReports();
+    }
+
+    public static String getLogFilePath() {
+        File document = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        return new File(document.toString() + "/NaviToTesla.log").toString();
+    }
+
+    private static void appendLog(String logLevel, String message) {
+        String dateTime = dateFormatter.format(Calendar.getInstance().getTime());
+        String text = String.format("%s %s %s", dateTime, logLevel, message);
+
+        File document = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        if (!document.exists() && !document.mkdirs()) {
+            firebaseCrashlytics.log("create document directory fail");
+            return;
+        }
+
+        File file = new File(document.toString() + "/NaviToTesla.log");
+
+        try (BufferedWriter buf = new BufferedWriter(new FileWriter(file, true))) {
+            buf.append(text);
+            buf.newLine();
+        } catch (IOException e) {
+            recordException(e);
+        }
+    }
+
+    public static long getLogFileSize() {
+        File document = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(document.toString() + "/NaviToTesla.log");
+        if (!file.exists()) {
+            return 0L;
+        }
+        return file.length();
+    }
+
+    public static void deleteLogFile() {
+        File document = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(document.toString() + "/NaviToTesla.log");
+        if (file.exists()) {
+            file.delete();
+        }
     }
 }
