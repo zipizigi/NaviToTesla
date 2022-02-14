51bc454c1aa99e9b400865ae1a033edd8dffa546
zipi <6834751+zipizigi@users.noreply.github.com>
2022-02-14 18:36:47 +0900

    API Call change to WorkManager

---
+++ app/src/main/java/me/zipi/navitotesla/background/VersionCheckWorker.java
@@ -0,0 +1,39 @@
+package me.zipi.navitotesla.background;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Constraints;
+import androidx.work.ListenableWorker;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+import me.zipi.navitotesla.util.AnalysisUtil;
+import me.zipi.navitotesla.util.AppUpdaterUtil;
+
+public class VersionCheckWorker extends Worker {
+
+    public VersionCheckWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    public static void startVersionCheck(@NonNull Context context) {
+        AnalysisUtil.log("start version check worker");
+        WorkRequest workRequest = new OneTimeWorkRequest.Builder(VersionCheckWorker.class)
+                .setConstraints(new Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.CONNECTED)
+                        .build())
+                .build();
+        WorkManager.getInstance(context).enqueue(workRequest);
+    }
+
+    @NonNull
+    @Override
+    public ListenableWorker.Result doWork() {
+        AppUpdaterUtil.notification(getApplicationContext());
+        return Result.success();
+    }
+}
