84389368a4d61920aa43d0418271f8d0064bc7ff
HongSeok Kim <hongseok.kim@navercorp.com>
2023-11-15 20:20:44 +0900

    ktlin 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     id("com.google.gms.google-services") version "4.3.13" apply false
     id("com.google.firebase.crashlytics") version "2.9.1" apply false
     id("com.google.firebase.firebase-perf") version "1.4.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
 }
