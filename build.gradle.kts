e4bae5a7214d67392a0deccf3ccd16c8c8a2989f
HongSeok Kim <6834751+zipizigi@users.noreply.github.com>
2023-11-15 10:37:49 +0900

    build.gradle.kts 변환, 의존성 업데이트

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.\
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+
+    id("com.google.gms.google-services") version "4.3.13" apply false
+    id("com.google.firebase.crashlytics") version "2.9.1" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.1" apply false
+}
