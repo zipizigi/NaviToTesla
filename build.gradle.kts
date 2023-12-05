28406a745db05a8f381e104af0aaab6ce634fb97
HongSeok Kim <hongseok.kim@navercorp.com>
2023-12-05 17:18:09 +0900

    change to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.\
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
-
-    id("com.google.gms.google-services") version "4.4.0" apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
-    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.0.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.google.gms.service) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
+    alias(libs.plugins.ktlint) apply false
 }
