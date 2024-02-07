36c45a13ca4f7e81a7a456c2fc927e6da99f7527
김홍석[Biz Platform Dev] <hongseok.kim@navercorp.com>
2024-02-07 18:25:57 +0900

    fix: share by app bug fix (when not korean)

--- app/src/main/kotlin/me/zipi/navitotesla/db/AppDatabase.kt
+++ app/src/main/kotlin/me/zipi/navitotesla/db/AppDatabase.kt
@@ -9,11 +9,12 @@ import androidx.room.TypeConverters
 
 @Database(
     entities = [PoiAddressEntity::class, ConditionEntity::class],
-    version = 4,
+    version = 5,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4),
+        AutoMigration(from = 4, to = 5),
     ],
 )
 @TypeConverters(DateConverter::class)
--- app/src/main/kotlin/me/zipi/navitotesla/service/share/TeslaShareByApp.kt
+++ app/src/main/kotlin/me/zipi/navitotesla/service/share/TeslaShareByApp.kt
@@ -5,17 +5,30 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import me.zipi.navitotesla.BuildConfig
+import me.zipi.navitotesla.model.Poi
 import me.zipi.navitotesla.util.AnalysisUtil
+import java.util.Locale
 
 class TeslaShareByApp(context: Context) : TeslaShareBase(context), TeslaShare {
-    override suspend fun share(address: String) {
+    override suspend fun share(poi: Poi) {
         AnalysisUtil.log("share using tesla app share")
         val intent = Intent()
         intent.action = "android.intent.action.SEND"
         intent.component = ComponentName("com.teslamotors.tesla", "com.tesla.share.ShareActivity")
         intent.type = "text/plain"
+        val address = if (Locale.getDefault().language.equals("ko", ignoreCase = true) || poi.isGpsEmpty()) {
+            poi.getRoadAddress()
+        } else {
+            poi.getGpsAddress()
+        }
         intent.putExtra("android.intent.extra.TEXT", address)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        if (BuildConfig.DEBUG) {
+            AnalysisUtil.makeToast(context, "[DEBUG] 목적지 전송 By App Skip\n$address")
+            return
+        }
         try {
             context.startActivity(intent)
             AnalysisUtil.logEvent("share_by_app_success", Bundle())
